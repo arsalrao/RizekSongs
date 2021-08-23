75700b9ae1552a7b8f1d0be1d651a7ed179c6683
Rao Arsalan <raoarsalan30@hotmail.com>
2021-08-23 10:44:43 +0400

    Add: handle back button

--- features/songs/src/main/java/com/raoarsalan/songs/viewmodel/TrackDetailsViewModel.kt
+++ features/songs/src/main/java/com/raoarsalan/songs/viewmodel/TrackDetailsViewModel.kt
@@ -1,8 +1,14 @@
 package com.raoarsalan.songs.viewmodel
 
+import com.raoarsalan.core.utils.NavigationCommand
 import com.raoarsalan.core.viewmodel.BaseViewModel
 
 
 class TrackDetailsViewModel : BaseViewModel() {
+
+
+    fun select() {
+        navigationCommands.value = NavigationCommand.Back
+    }
 
 }
